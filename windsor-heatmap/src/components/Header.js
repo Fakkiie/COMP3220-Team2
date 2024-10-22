5e38475e2b4ffccd62783eab082a91a6d1b62ceb
Fakkiie <landonhadre04@gmail.com>
2024-10-22 13:00:17 -0400

    Adding upload file feature

--- windsor-heatmap/src/components/Header.js
+++ windsor-heatmap/src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     <header className="bg-gray-800 text-white p-3 shadow-sm">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-medium">SR Dashboard</h1>
+        <h1>Upload Data</h1>
       </div>
     </header>
   );
---
+++ windsor-heatmap/src/components/Upload.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Upload = () =>{
+    return (
+        <h1>test</h1>
+    );
+};
+
+export default Upload
