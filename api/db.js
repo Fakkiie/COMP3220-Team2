9c20b0a8d9da06ed73c211ce8fdbf910405710a0
Fakkiie <landonhadre04@gmail.com>
2024-11-06 13:33:19 -0500

    this better work

--- api/db.js
+++ api/db.js
@@ -1,12 +1,14 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const { Pool } = pkg;
+
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
   },
 });
 
